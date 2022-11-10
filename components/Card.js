1d69611a7a586fafdc9a1f6f8d415e1cbdbac801
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-11-10 15:09:38 +0100

    changes as given in #2

---
+++ components/Card.js
@@ -0,0 +1,14 @@
+export default function Card() {
+
+    return (
+        <div>
+            <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Morty" />
+
+            <h2>Morty Smith</h2>
+
+        </div>
+        
+
+    )
+
+}
---
+++ components/Navigation.js
@@ -0,0 +1,10 @@
+export default function Header() {
+
+    return (
+        <h2>Navigation</h2>
+        
+
+    )
+
+}
+
